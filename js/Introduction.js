857f236f980a463b8fdf8438e236974d13fd79c3
ginasun28 <83609336+ginasun28@users.noreply.github.com>
2022-06-17 22:17:44 -0700

    Revised introduction page

--- js/Introduction.js
+++ js/Introduction.js
@@ -28,7 +28,59 @@ const infoData = [
         picture1: "image/info-jkt.jpeg",
         description2: "The history of Jakarta dates back to at least the 14th Century with the development of a small port of the Hindu Pajajaran kingdom at the mouth of the Ciliwung river. Searching for the fabled spice islands, the Portuguese were the first Europeans to arrive and establish a fortress on the site in the early 16th Century. The old port was attacked by a neighboring sultanate under the leadership of Prince Fatahillah. After the assault, the Portuguese navy fleet was destroyed. Fatahillah changed the name of the Sunda Kelapa port to Jayakarta, meaning “Total Victory”. It was to this town that Dutch spice merchants came in the late 16th Century and began a trading association with Europe that was to dictate the history of Jakarta, and Indonesia as a nation, for nearly 350 years. Under the Dutch East India Company (VOC) the town of Jayakarta was renamed to Batavia in 1619; from here they ruled Indonesia for more than three centuries. Following the Japanese invasion and rule of the country from 1942-45, on August 17,1945, Indonesia's first president Soekarno, proclaimed Indonesian independence and Jakarta became the accepted nation's capital.",
         picture2: "image/info-jkt2.jpeg"
+    },
+
+    {
+        id: 4,
+        title: "Introduction : Tokyo",
+        name: "tokyo",
+        description1: "Tokyo is Japan's capital and the world's most populous metropolis. It is also one of Japan's 47 prefectures, consisting of 23 central city wards and multiple cities, towns and villages west of the city center. The Izu and Ogasawara Islands are also part of Tokyo.",
+        picture1: "image/info-tyo1.jpeg",
+        description2: "Prior to 1868, Tokyo was known as Edo. Previously a small castle town, Edo became Japan's political center in 1603 when Tokugawa Ieyasu established his feudal government there. A few decades later, Edo had grown into one of the world's largest cities. With the Meiji Restoration of 1868, the emperor and capital moved from Kyoto to Edo, which was renamed Tokyo (Eastern Capital). Large parts of Tokyo were destroyed in the Great Kanto Earthquake of 1923 and the air raids of 1945. Today, Tokyo offers a seemingly unlimited choice of shopping, entertainment, culture and dining to its visitors. The city's history can be appreciated in districts such as Asakusa and in many excellent museums, historic temples and gardens. Contrary to common perception, Tokyo also offers a number of attractive green spaces in the city center and within relatively short train rides at its outskirts.",
+        picture2: "image/info-tyo2.jpeg"
+    },
+
+    {
+        id: 5,
+        title: "Introduction : Taipei",
+        name: "taipei",
+        description1: "Taipei is the political, economic, educational and cultural center of Taiwan, with a population of 2.5 million. The region of Taipei had been mainly inhabited by the Ketagalan tribe prior to the large influx of mainland Chinese immigrants in the 17th century. Under the Qing Dynasty of China, Taipei was made the capital of Taiwan in 1887 when the island was incorporated into a Chinese province. Following the first Sino-Japanese War of 1895, Taiwan was annexed by Japan. The Japanese colonized the island until 1945, when it was returned to China after the World War II.",
+        picture1: "image/info-tpe1.jpeg",
+        description2: "Taipei is a home to various architectural and cultural landmarks, such as Taipei 101, Chiang Kai-shek Memorial Hall, Longshan Temple, Presidential Office Building, Taipei Guest House, Ximending and several night markets dispersed throughout the city. Liberty Square is the political and cultural center of the city. At its east end stands the National Chiang Kai-shek Memorial Hall, which is flanked by the National Concert Hall on the north and the National Theater on the south. The square is a place for public gatherings, as well as a home to civic parks. Presidential Office Building and Taipei Guest House are the two historical buildings constructed during the Japanese colonial period. Designed by Japanese architects, both buildings feature a Baroque style that clearly takes inspiration from the classical European architecture. No visit to Taipei is complete without a trip to one of its night markets. At the Ximending Night Market you will find hundreds of stalls selling clothes, accessories, makeup products, and traditional Taiwanese snacks. The latter are particularly tempting while watching an array of shows by the local street performers – an experience not to be forgotten for a long time! Taipei is a fascinating place which blends seamlessly ancient Chinese traditions with modern trends. Follow this self-guided walking tour to see the key landmarks of Taipei and appreciate the city's vibrant culture in its entirety.",
+        picture2: "image/info-tpe2.jpeg"
+    },
+
+    {
+        id: 6,
+        title: "Introduction : Granville Island",
+        name: "granville island",
+        description1: "In the early 1900s, Granville Island was home to factories, plants and sawmills. Things are a little different today—Granville Island is both a locals’ favorite and a huge draw for visitors. Technically a sandspit and not an island, the neighbourhood sits just south of the downtown peninsula, right under the Granville Bridge. The Granville Island Public Market acts as a hub of activity, but it’s also one of the city’s most important cultural districts with theatres, artisan workshops and craft studios. One of the best ways to get to Granville Island is one of the adorable mini-tugboat ferries that criss-cross False Creek – they’re favourites among young and old! Once you’re there, the biggest attraction on Granville Island is the Public Market. Housed indoors, there are endless rows of stalls that feature fresh produce, gourmet foods, baked goods, and seafood. For the food-focused, a walking tour of the market can be a great way to get an insider’s insight. Outside of the market, catch a show at one of the many theatres on the island, browse an art gallery show, or appeal to your outdoorsy side with a kayaking or paddleboarding tour.",
+        picture1: "image/info-granvilleisland.jpeg",
+        description2: "The market itself is often described as a “food lover’s paradise,” and an impromptu picnic is easily picked up between vendors offering cheese, charcuterie, bread and fresh produce. If you’re looking for something casual but already put together, try the popular food court at the end of the market building. If a restaurant is more your style, there are some great seafood offerings down here, and there’s nothing like dining by the waterfront. Don’t forget to fill your glass while you’re down here! The area is home to an artisan sake maker (the first in Canada), a spirits distillery, and two breweries. With a mix of unique crafts, skilled artisans, outdoor outfitters and deluxe gourmet stores, Granville Island is one of the best places in the city to purchase souvenirs for back home. The public market is a great place to start; pick up smoked salmon, exclusive teas, and other gourmet goodies. Check out the stores in the Net Loft building for First Nations artworks, B.C. wines, and other unique gifts. Take a stroll along Railspur Alley and the far end of the island to peek into artisan studios where glassblowers, potters, jewellers and even a broom maker ply their crafts.",
+        picture2: "image/info-granvilleisland2.jpeg"
+    },
+
+    {
+        id: 7,
+        title: "Introduction : Ximending",
+        name: "ximending",
+        description1: "Located in west Taipei, Ximending is one of the most popular tourist spots among both local and international travelers. Ximending features a wide selection of fashionable clothing and accessories in many styles include Japanese, Chinese and Western. Ximending is similar to Shibuya in Japan, both are famed for a diversity of entertainments and activities that allure large numbers of young people and young adults. It is often a gathering site for young people and has been noted the most fashionable and popular shopping area in Taiwan.",
+        picture1: "image/info-ximending.jpeg",
+        description2: "Ximending is filled with all kinds of shopping options, from department stores to shops and stores, including various CD shops, hair salons, Eslite Department Stores, movie theaters, Karaoke and many others. Other than enjoying various activities, do not miss the chance to sample famous dishes of Ximending such as “Ya Rou Bian” for tasty duck meat, “Ay-Chung Flour-Rice Noodle”  for an unique experience appreciating delicious rice noodles in long line, “Simonfood Tempura” for mouth-watering Taiwanese style tempura, “Lautianlu” for stewed and braised food, plus many other delicacies that will satisfy  your taste buds. Ximending is often crowded by visitors as there are abundant options of shops and restaurants. Being a hot spot in Taipei, a lot of celebrities choose to hold concerts and events at the Ximending Walking Street. With an excellent location close to Taipei Main Station, Ximending offers great convenience in transportations with MRT and a multitude of buses. For travelers coming from other cities, they may simply take train or THSR (Taiwan High Speed Rail) to Taipei Main Station and then take a short MRT ride or 10mins short walk to Ximending area.",
+        picture2: "image/info-ximending2.jpeg"
+    },
+
+    {
+        id: 8,
+        title: "Introduction : Merdeka Square",
+        name: "merdeka square",
+        description1: "As one of the largest public squares in the world, Merdeka Square is considered a must-visit attraction in Jakarta. Boasting of open green spaces shaded by a tree cover, well-paved roads winding all around, and Indonesia’s National Monument at its centre, it’s certainly a leisure destination for those looking to spend a quiet day exploring Jakarta City. One merely needs to visit Merdeka Square to get a glimpse into Jakarta and Indonesia’s rich history, with plenty of attractions within walking distance.",
+        picture1: "image/info-merdeka square.jpeg",
+        description2: "With no entry fee, Merdeka Square is open to the public for all hours of the day. It is known to be quite busy as a location for local fairs, artisan markets, and even military parades. Aside from its sprawling lawns, the grand Merdeka Square includes a deer enclosure, a musical fountain, and sports and recreation spaces in its 75-hectare space. Located at the centre of Jakarta City, Merdeka Square is known for its historical, cultural, and architectural importance. From watching the deer roam free in their enclosure to enjoying a family picnic or admiring Jakarta from atop the National Monument, there are plenty of activities at Merdeka Square to keep you busy for hours!",
+        picture2: "image/info-merdeka square2.jpeg"
     }
+
+
 ]
 
 // localstorge of data
@@ -55,18 +107,18 @@ function parseUrl(url) {
     return result;
 }
 
-// search city
+// search city for infoData
 function search(city) {
     return infoData.filter(location => (location["name"].includes(city)
     ));
 }
 
-document.querySelector('.search-btn').addEventListener('click',
-    function () {
-    const value = document.querySelector('.search-input').value
-    localStorage.setItem('location', value);
-    }
-)
+// document.querySelector('.search-btn').addEventListener('click',
+//     function () {
+//     const value = document.querySelector('.search-input').value
+//     localStorage.setItem('location', value);
+//     }
+// )
 
 // Display introduction of city
 function infoDisplay() {
